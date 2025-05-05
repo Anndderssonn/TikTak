bd355a5f1b2b163aa1546438e8abbe3aeabbc15f
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-05-04 19:45:25 -0500

    feat: Add assets and config files

---
+++ lib/shared/data/local_video_post.dart
@@ -0,0 +1,62 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Recharging in the sun.',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 23230,
+    'views': 1523,
+  },
+  {
+    'name': 'A recipe for every day.',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 24230,
+    'views': 1343,
+  },
+  {
+    'name': 'How peaceful this place is.',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 21564320,
+    'views': 123563,
+  },
+  {
+    'name': 'What type of plant is this?',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 320,
+    'views': 2300,
+  },
+  {
+    'name': 'Working with a lot of style.',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 3230,
+    'views': 31030,
+  },
+  {
+    'name': 'I came to Japan just to see this!',
+    'videoUrl': 'assets/videos/6.mp4',
+    'likes': 10,
+    'views': 330,
+  },
+  {
+    'name': 'I have a lot of tidying up to do. XD',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': 1320,
+    'views': 33032,
+  },
+  {
+    'name': 'How far will I go this time?',
+    'videoUrl': 'assets/videos/8.mp4',
+    'likes': 342,
+    'views': 3332,
+  },
+  {
+    'name': 'Escalators to the sky.',
+    'videoUrl': 'assets/videos/9.mp4',
+    'likes': 342,
+    'views': 3332,
+  },
+  {
+    'name': 'Playing with the shadow.',
+    'videoUrl': 'assets/videos/10.mp4',
+    'likes': 342,
+    'views': 3332,
+  },
+];
