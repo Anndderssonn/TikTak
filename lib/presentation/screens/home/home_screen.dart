1849944352b034cfdb5551dd37643caea3999b16
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-05-04 22:10:46 -0500

    feat: Add VideoScrollableView widget

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tiktak/presentation/providers/home/home_provider.dart';
+import 'package:tiktak/presentation/widgets/shared/video_scrollable_view.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Home Screen!')));
+    final homeProvider = context.watch<HomeProvider>();
+
+    return Scaffold(
+      body:
+          homeProvider.initialLoading
+              ? const Center(child: CircularProgressIndicator())
+              : VideoScrollableView(videos: homeProvider.videos),
+    );
   }
 }
